9b4c71f873e709663c4645204579baf8352b3522
nother <jtg@nother.net>
2012-07-29 23:02:25 -0500

    check for virtualenvwrapper

--- home/.profile.d/virtualenv.sh
+++ home/.profile.d/virtualenv.sh
@@ -1,2 +1,4 @@
 export WORKON_HOME=$HOME/.virtualenvs
-source /usr/local/bin/virtualenvwrapper.sh
+if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
+    source /usr/local/bin/virtualenvwrapper.sh
+fi
